741d1a62fbb7de8f577387397a23630b06da1487
zioncmyk <53898789+zioncmyk@users.noreply.github.com>
2021-12-26 12:20:34 +0700

    build pertama

---
+++ site/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        PORTFOLIO
+      </div>
+      <div>
+        <button className="inline-block px-5 py-3 font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500">
+          Get full access
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
